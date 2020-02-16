5a533b5e7b602ce1bd25e07c336f361897702a48
Austinshepherd <austink.shepherd@gmail.com>
2020-02-16 12:48:20 -0800

    (DAY 47) mobile optimization day

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,9 @@
 import React from "react"
 import "../components/layout"
 
+
+
+
 const CardOne = () => (
     <div className="Cards_A">
     <div className="cardOne">
@@ -26,6 +29,7 @@ const CardOne = () => (
 <div className="cardThree">
         <div className="card_container">
             <div className="output_card_background">
+         
              <ul>
             {/*output*/} 
              </ul>
