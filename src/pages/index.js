a5949220558f0a1e5f6a8babc63ecc9c70559cc6
Austinshepherd <austink.shepherd@gmail.com>
2020-02-18 03:13:09 -0800

    Making changes for ui output and doing testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,48 @@
-import React from "react"
-
+import React, { Component } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import CardOne from "../components/card"
 import CardTwo from "../components/cardTwo"
 
 
+function useKey(key) {
+  
+  const [pressed, setPressed] = React.useState(false)
+
+ 
+  const match = event => key.toLowerCase() === event.key.toLowerCase()
+
+  const onDown = event => {
+      if (match(event)) setPressed(true)
+  }
+
+  const onUp = event => {
+      if (match(event)) setPressed(false)
+  }
+
+      React.useEffect(() => {
+      window.addEventListener("keydown", onDown)
+      window.addEventListener("keyup", onUp)
+      return () => {
+          window.removeEventListener("keydown", onDown)
+          window.removeEventListener("keyup", onUp)
+      }
+  }, [key])
+
+  return pressed
 
 
-const IndexPage = () => (
+  }
+
+  
+
+class App extends Component {
+  
+  render() {
+    const shift = useKey("shift")
+
+
+  return (
   <Layout>
     <SEO title="Home" />
     <div className="header_background">
@@ -16,6 +50,9 @@ const IndexPage = () => (
     <p className="SiteSubTitle">A default CSS font generator</p>
     <h1 className="SiteTitle">FontsGo</h1>
     </div>
+
+    {shift && (
+      <>
     <div className="form_container">
       <form>
         <p className="input_lbl">Type here:</p>
@@ -24,13 +61,16 @@ const IndexPage = () => (
         </div>
       </form>
     </div>
+    </>
+    )}
     </div>
     <CardOne />
     <CardTwo />
   </Layout>
-)
+  );
+  }
+  } 
 
+  export default App
 
-
-export default IndexPage
 
