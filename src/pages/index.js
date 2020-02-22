5475d0b86eb1cf2ced74b83bc312a55cddafb88d
Austinshepherd <austink.shepherd@gmail.com>
2020-02-21 23:40:39 -0800

    (DAY 52) Working on output

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,44 +5,9 @@ import CardOne from "../components/card"
 import CardTwo from "../components/cardTwo"
 
 
-function useKey(key) {
+
+const Home = () => (
   
-  const [pressed, setPressed] = React.useState(false)
-
- 
-  const match = event => key.toLowerCase() === event.key.toLowerCase()
-
-  const onDown = event => {
-      if (match(event)) setPressed(true)
-  }
-
-  const onUp = event => {
-      if (match(event)) setPressed(false)
-  }
-
-      React.useEffect(() => {
-      window.addEventListener("keydown", onDown)
-      window.addEventListener("keyup", onUp)
-      return () => {
-          window.removeEventListener("keydown", onDown)
-          window.removeEventListener("keyup", onUp)
-      }
-  }, [key])
-
-  return pressed
-
-
-  }
-
-  
-
-class App extends Component {
-  
-  render() {
-    const shift = useKey("shift")
-
-
-  return (
   <Layout>
     <SEO title="Home" />
     <div className="header_background">
@@ -51,8 +16,6 @@ class App extends Component {
     <h1 className="SiteTitle">FontsGo</h1>
     </div>
 
-    {shift && (
-      <>
     <div className="form_container">
       <form>
         <p className="input_lbl">Type here:</p>
@@ -61,16 +24,12 @@ class App extends Component {
         </div>
       </form>
     </div>
-    </>
-    )}
     </div>
     <CardOne />
     <CardTwo />
   </Layout>
-  );
-  }
-  } 
+)
 
-  export default App
+  export default Home
 
 
