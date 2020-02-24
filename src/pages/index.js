84ba70546828912f3d80d439a258c1323621ebc2
Austinshepherd <austink.shepherd@gmail.com>
2020-02-24 01:53:25 -0800

    Finished handleKeyPress event

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,48 @@
 import React from "react"
-import KeyboardEventHandler from 'react-keyboard-event-handler';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import CardOne from "../components/card"
 import CardTwo from "../components/cardTwo"
 
 
+export default class IndexPage extends React.Component {
+  state = {
+    TextName: "",
+  }
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
+    })
+  }
+  handleKeyPress = event => {
+    event.preventDefault()
+    alert(`Welcome ${this.state.TextName}`)
+  }
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <div className="header_background">
+          <div className="title_container">
+            <p className="SiteSubTitle">A default CSS font generator</p>
+            <h1 className="SiteTitle">FontsGo</h1>
+          </div>
+          </div>
+          <form>
 
-const Home = (props) => (
-  
-  <Layout>
-    <SEO title="Home" />
-    <div className="header_background">
-    <div className="title_container">
-    <p className="SiteSubTitle">A default CSS font generator</p>
-    <h1 className="SiteTitle">FontsGo</h1>
-    </div>
-
-    <div className="form_container">
-    <KeyboardEventHandler
-    handleKeys={['a', 'b', 'c']}
-    onKeyEvent={(key, e) => console.log(`do something upon keydown event of ${key}`)} >
-      <form>
-        <p className="input_lbl">Type here:</p>
-        <div className="textarea-border">
-        <textarea rows="5" cols="120"></textarea>
-        </div>
-      </form>
-      </KeyboardEventHandler>
-    </div>
-    </div>
-    <CardOne />
-    <CardTwo />
-  </Layout>
-)
-
-  export default Home
-
-
+            <textarea
+              type="text"
+              name="TextName"
+              value={this.state.Text}
+              onKeyPress={this.handleKeyPress}
+            />
+          </form>
+          <CardOne />
+          <CardTwo />
+      </Layout>
+    )
+  }
+}
